554a4afdcf799ce22b40dab25ccd3a70fc13090a
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-06 16:33:26 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,3 +1,11 @@
+# lib/practicing_returns.rb
+=begin
+  #hello(array) uses a while loop to iterate over array, yielding to each element of the array.
+  it captures the return value of using yield 
+  and returns the values of yielding those items to the blocks
+  #hello(array) works the same as the #collect method does. 
+=end
+ 
 require 'pry'
 
 def hello(array)
